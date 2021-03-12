0087d5076d00a55829b90d93eb2c47a03de3c605
ryoichi <ryoichi.noguchi@gmail.com>
2021-03-12 19:39:08 +0900

    Add stanford lecture3 demo

---
+++ lecture3/lecture3/hello/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "hello/index.html")
+
+def brian(request):
+    return HttpResponse("hello brian")
+
+def greet(request, name):
+    return HttpResponse(f"hello, {name.capitalize()}")
